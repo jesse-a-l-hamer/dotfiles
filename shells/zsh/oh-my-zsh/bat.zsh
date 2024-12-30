249a9e74ce0c9cdd208f8bab02cba08478b10f82
Jesse Hamer <jhamer90811@gmail.com>
2024-12-30 17:25:13 -0600

    removed rvm and any references to it

--- shells/zsh/oh-my-zsh/bat.zsh
+++
@@ -1,2 +0,0 @@
-alias cat='bat'
-alias -g -- --help='--help 2>&1 | bat --language=help --style=plain'
--- shells/zsh/oh-my-zsh/completions.zsh
+++
@@ -1,1 +0,0 @@
-eval "$(curl -fs https://raw.githubusercontent.com/99designs/aws-vault/master/contrib/completions/zsh/aws-vault.zsh)"
