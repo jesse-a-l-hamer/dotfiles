146ae1e945f0389f419f8684f892196a0a26d401
Jesse Hamer <jhamer90811@gmail.com>
2025-01-29 13:11:51 -0600

    chore(kickstart-nvim): remove unnecessary comments

--- editor/kickstart-nvim/lua/kickstart/plugins/mini.lua
+++ editor/kickstart-nvim/lua/kickstart/plugins/mini.lua
@@ -8,14 +8,6 @@ return {
 
       require("mini.surround").setup()
       vim.keymap.set({ "n", "x" }, "s", "<Nop>")
-
-      -- local statusline = require "mini.statusline"
-      -- statusline.setup { use_icons = vim.g.have_nerd_font }
-      --
-      -- ---@diagnostic disable-next-line: duplicate-set-field
-      -- statusline.section_location = function()
-      --   return "%2l:%-2v"
-      -- end
     end,
   },
 }
