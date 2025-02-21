ef42217c5c0656902dc765eb25c691ff676bcc56
Jesse Hamer <jhamer90811@gmail.com>
2025-02-21 13:13:14 -0600

    feat(editor): add monaspace plugin

---
+++ editor/neovim/nvim/lua/plugins/monaspace.lua
@@ -0,0 +1,13 @@
+return {
+  "jackplus-xyz/monaspace.nvim",
+  lazy = false,
+  opts = {
+    style_map = {
+      bold = {},
+      italic = {
+        Comment = true,
+      },
+      bold_italic = {},
+    }
+  },
+}
