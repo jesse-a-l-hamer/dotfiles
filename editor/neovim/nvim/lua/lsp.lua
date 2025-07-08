598dbc6a8b550a5d347ca312bedc21b6f924c1d2
Jesse Hamer <jhamer90811@gmail.com>
2025-07-08 12:28:39 -0500

    refactor(editor/neovim): add lua/lsp.lua

---
+++ editor/neovim/nvim/lua/lsp.lua
@@ -0,0 +1,59 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+  group = vim.api.nvim_create_augroup("kickstart-lsp-attach", { clear = true }),
+  callback = function(event)
+    local map = function(keys, func, desc, mode)
+      mode = mode or "n"
+      vim.keymap.set(mode, keys, func, { buffer = event.buf, desc = desc })
+    end
+
+    map("<leader>lrs", ":IncRename ", "[s]ymbol", { "n", "x" })
+    map("<leader>la", vim.lsp.buf.code_action, "code [a]ction", { "n", "x" })
+
+    local client = vim.lsp.get_client_by_id(event.data.client_id)
+    if client and client.supports_method(vim.lsp.protocol.Methods.textDocument_documentHighlight) then
+      local highlight_augroup = vim.api.nvim_create_augroup("kickstart-lsp-highlight", { clear = false })
+      vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.document_highlight,
+      })
+
+      vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
+        buffer = event.buf,
+        group = highlight_augroup,
+        callback = vim.lsp.buf.clear_references,
+      })
+
+      vim.api.nvim_create_autocmd("LspDetach", {
+        group = vim.api.nvim_create_augroup("kickstart-lsp-detach", { clear = true }),
+        callback = function(event2)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = "kickstart-lsp-highlight", buffer = event2.buf }
+        end,
+      })
+    end
+  end,
+})
+
+-- Change diagnostic symbols in the sign column (gutter)
+if vim.g.have_nerd_font then
+  local signs = { ERROR = "", WARN = "", INFO = "", HINT = "" }
+  local diagnostic_signs = {}
+  for type, icon in pairs(signs) do
+    diagnostic_signs[vim.diagnostic.severity[type]] = icon
+  end
+  vim.diagnostic.config { signs = { text = diagnostic_signs } }
+end
+
+-- enable lsps
+vim.lsp.enable "bashls"
+vim.lsp.enable "beancount"
+vim.lsp.enable "jinja_lsp"
+vim.lsp.enable "lua_ls"
+vim.lsp.enable "markdown_oxide"
+vim.lsp.enable "marksman"
+vim.lsp.enable "taplo"
+vim.lsp.enable "yamlls"
+vim.lsp.enable "basedpyright"
+vim.lsp.enable "ruff"
+vim.lsp.enable "pylsp"
