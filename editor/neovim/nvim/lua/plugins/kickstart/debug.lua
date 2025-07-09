c6a49277d3c7f9ed72fb36a3e425bb330ff61401
Jesse Hamer <jhamer90811@gmail.com>
2025-07-09 13:37:38 -0500

    feat(editor/neovim): clean debug.lua

--- editor/neovim/nvim/lua/plugins/kickstart/debug.lua
+++ editor/neovim/nvim/lua/plugins/kickstart/debug.lua
@@ -1,31 +1,11 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
 return {
-  -- NOTE: Yes, you can install new plugins here!
   "mfussenegger/nvim-dap",
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
-    -- Creates a beautiful debugger UI
     "rcarriga/nvim-dap-ui",
-
     -- Required dependency for nvim-dap-ui
     "nvim-neotest/nvim-nio",
-
-    -- Installs the debug adapters for you
-    "williamboman/mason.nvim",
-    "jay-babu/mason-nvim-dap.nvim",
-
-    -- Add your own debuggers here
-    "leoluz/nvim-dap-go",
   },
   keys = {
-    -- Basic debugging keymaps, feel free to change to your liking!
     {
       "<leader>dd",
       function()
@@ -81,29 +61,9 @@ return {
     local dap = require "dap"
     local dapui = require "dapui"
 
-    require("mason-nvim-dap").setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
-      automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
-      ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        "delve",
-      },
-    }
-
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
     dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
       icons = { expanded = "▾", collapsed = "▸", current_frame = "*" },
       controls = {
         icons = {
@@ -124,7 +84,7 @@ return {
     -- vim.api.nvim_set_hl(0, 'DapBreak', { fg = '#e51400' })
     -- vim.api.nvim_set_hl(0, 'DapStop', { fg = '#ffcc00' })
     local breakpoint_icons = vim.g.have_nerd_font
-        and { Breakpoint = "", BreakpointCondition = "", BreakpointRejected = "", LogPoint = "", Stopped = "" }
+        and { Breakpoint = "", BreakpointCondition = "", BreakpointRejected = "", LogPoint = "", Stopped = " " }
       or { Breakpoint = "●", BreakpointCondition = "⊜", BreakpointRejected = "⊘", LogPoint = "◆", Stopped = "⭔" }
     for type, icon in pairs(breakpoint_icons) do
       local tp = "Dap" .. type
