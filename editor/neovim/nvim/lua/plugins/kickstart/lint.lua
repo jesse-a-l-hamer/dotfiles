1865ea1e6ee280554fdfddb0585785a04923cbdd
Jesse Hamer <jhamer90811@gmail.com>
2025-07-09 12:43:49 -0500

    feat(editor/neovim): add linter configs

--- editor/neovim/nvim/lua/plugins/kickstart/lint.lua
+++ editor/neovim/nvim/lua/plugins/kickstart/lint.lua
@@ -1,12 +1,18 @@
 return {
 
-  { -- Linting
+  {
     "mfussenegger/nvim-lint",
     event = { "BufReadPre", "BufNewFile" },
     config = function()
       local lint = require "lint"
       lint.linters_by_ft = {
-        markdown = { "markdownlint" },
+        markdown = { "vale" },
+        shell = { "shellcheck" },
+        bash = { "shellcheck" },
+        rst = { "vale" },
+        tex = { "vale" },
+        text = { "vale" },
+        json = { "jsonlint" },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
@@ -30,15 +36,15 @@ return {
       -- }
       --
       -- You can disable the default linters by setting their filetypes to nil:
-      -- lint.linters_by_ft['clojure'] = nil
-      -- lint.linters_by_ft['dockerfile'] = nil
-      -- lint.linters_by_ft['inko'] = nil
-      -- lint.linters_by_ft['janet'] = nil
+      lint.linters_by_ft['clojure'] = nil
+      lint.linters_by_ft['dockerfile'] = nil
+      lint.linters_by_ft['inko'] = nil
+      lint.linters_by_ft['janet'] = nil
       -- lint.linters_by_ft['json'] = nil
       -- lint.linters_by_ft['markdown'] = nil
       -- lint.linters_by_ft['rst'] = nil
-      -- lint.linters_by_ft['ruby'] = nil
-      -- lint.linters_by_ft['terraform'] = nil
+      lint.linters_by_ft['ruby'] = nil
+      lint.linters_by_ft['terraform'] = nil
       -- lint.linters_by_ft['text'] = nil
 
       -- Create autocommand which carries out the actual linting
