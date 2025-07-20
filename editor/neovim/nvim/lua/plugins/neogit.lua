7df5fde45d8d5e5c1eeaf0eff9ec0f4b7f7c95c8
Jesse Hamer <jhamer90811@gmail.com>
2025-07-20 13:29:03 -0500

    feat(editor/neovim): add Neogit plugin to config

---
+++ editor/neovim/nvim/lua/plugins/neogit.lua
@@ -0,0 +1,12 @@
+return {
+  "NeogitOrg/neogit",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+    "folke/snacks.nvim"
+  },
+  opts = {
+    graph_style = "kitty",
+    process_spinner = true,
+  }
+}
