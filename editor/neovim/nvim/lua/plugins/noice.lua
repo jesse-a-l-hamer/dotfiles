3fdb31fa6c0d571a323316f234414f606690c890
Jesse Hamer <jhamer90811@gmail.com>
2025-07-20 13:53:48 -0500

    feat(editor/neovim): stop using noice for hover and signature help

--- editor/neovim/nvim/lua/plugins/noice.lua
+++ editor/neovim/nvim/lua/plugins/noice.lua
@@ -7,18 +7,16 @@ return {
   ---@module 'noice'
   opts = { ---@type NoiceConfig
     lsp = {
-      override = {
-        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-        ["vim.lsp.util.stylize_markdown"] = true,
-        -- ["cmp.entry.get_documentation"] = true,
-      },
+      -- handled by lua.util.lsp_hover; uses markview to render hover docs
+      hover = { enabled = false },
+      -- handled by blink.cmp
+      signature = { enabled = false },
     },
     presets = {
       bottom_search = true,
       command_palette = true,
       long_message_to_split = true,
       inc_rename = true,
-      lsp_doc_border = true,
     },
   },
 }
